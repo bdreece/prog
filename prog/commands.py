b470f3f1c826776609283d760522c4d92406f4b5
Brian Reece <bdreece@mtu.edu>
2021-12-15 21:17:21 -0500

    Did some runtime testing, fixed some bugs

--- prog/commands.py
+++ prog/commands.py
@@ -7,10 +7,10 @@ def fileType(ctx, param, value):
     if not isinstance(ctx.obj, dict):
         ctx.obj = dict()
 
-    if str(param)[-1] == 'l':
-        ctx.obj['filetype'] = str(param)[-3:-1]
-    else:
+    if str(param)[-2] == 'l':
         ctx.obj['filetype'] = str(param)[-4:-1]
+    else:
+        ctx.obj['filetype'] = str(param)[-5:-1]
 
 def selectFile(ctx, param, value):
     if not value or ctx.resilient_parsing:
@@ -19,7 +19,14 @@ def selectFile(ctx, param, value):
     if not isinstance(ctx.obj, dict):
         ctx.obj = dict()
 
-    ctx.obj['path'] = click.format_filename(value)
+    ctx.obj['path'] = str(click.format_filename(value))
+    if ctx.obj['path'][-3:] == 'yml':
+        ctx.obj['filetype'] = 'yml'
+    elif ctx.obj['path'][-4:] == 'json':
+        ctx.obj['filetype'] = 'json'
+    else:
+        click.echo('[ERROR] Unsupported file type')
+        ctx.exit()
 
 def editFile(ctx, param, value):
     if not value or ctx.resilient_parsing:
@@ -34,7 +41,7 @@ def editFile(ctx, param, value):
             ctx.obj['path'] = 'prog.yml'
             ctx.obj['filetype'] = 'yml'
         else:
-            click.echo('File not found')
+            click.echo('[ERROR] config file not found')
             ctx.exit()
 
     
@@ -52,9 +59,9 @@ def genFile(ctx, param, value):
         ctx.obj['path'] = 'prog.' + ctx.obj['filetype']
     elif 'filetype' not in ctx.obj.keys():
         if ctx.obj['path'][-1] == 'l':
-            ctx.obj['filetype'] = ctx.obj['path'][-5:-2]
+            ctx.obj['filetype'] = ctx.obj['path'][-3:]
         else:
-            ctx.obj['filetype'] = ctx.obj['path'][-6:-2]
+            ctx.obj['filetype'] = ctx.obj['path'][-4:]
     
     buffer = pkg_resources.resource_string(__name__, 'assets/prog.' + ctx.obj['filetype']).decode('utf-8')
     with open(ctx.obj['path'], 'wt') as f:

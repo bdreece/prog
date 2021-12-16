ff819e6c4a9b691b6b280aaae82c8b8519939a02
Brian Reece <bdreece@mtu.edu>
2021-12-15 21:37:57 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
 
+long_description = None
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='prog',
-    version='0.1.0',
-    py_modules=['prog.cli', 'prog.commands', 'prog.parse', 'prog.util'],
+    version='0.1.2',
+    author='Brian Reece',
+    author_email='bdreece@mtu.edu',
+    description='A tool for centralizing scripted shell commands via a configurable JSON or YAML file',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bdreece/prog',
     install_requires=['Click','pyyaml'],
+    packages=['prog'],
     package_data={'': ['assets/prog.json', 'assets/prog.yml'],},
     entry_points='''
         [console_scripts]
         prog=prog.cli:cli
-    ''',
+        ''',
+    python_requires='>=3.6'
 )

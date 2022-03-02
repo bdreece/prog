c0183611ee927490a19a78beee71659cb255bca2
Brian Reece <bdreece@mtu.edu>
2022-03-02 00:54:19 -0500

    Finalizing CLI

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,84 @@ use std::path::PathBuf;
 use std::string::String;
 use std::vec::Vec;
 
-use clap::{ArgEnum, Parser, Subcommand};
+use clap::{ArgEnum, Parser};
 
 #[derive(Parser, Debug)]
-#[clap(author = "Brian Reece", version = "v0.3-alpha", about = "A tool for centralizing scripted commands via a configurable markup file", long_about = None)]
+#[clap(
+    author = "Brian Reece",
+    version = "v0.3-alpha",
+    about = "A tool for centralizing scripted commands via a configurable markup file",
+    long_about = None
+)]
 struct Args {
-	#[clap(short, long, parse(from_os_str), value_name = "PATH", help = "Path to config file")]
+    #[clap(
+        short,
+        long,
+        parse(from_os_str),
+        value_name = "PATH",
+        help = "Path to config file"
+    )]
     path: Option<PathBuf>,
 
-	#[clap(short, long, arg_enum, value_name = "FORMAT", help = "Config file format")]
+    #[clap(
+        short,
+        long,
+        arg_enum,
+        group = "format_group",
+        value_name = "FORMAT",
+        help = "Config file format"
+    )]
     format: Option<Format>,
 
-    #[clap(subcommand)]
-    command: Command,
+	#[clap(
+        short,
+        long,
+        arg_enum,
+        group = "template_group",
+        value_name = "TEMPLATE",
+        help = "Config file template"
+    )]
+	template: Option<Template>,
 
-	#[clap(short, long, parse(from_occurrences), help = "Sets verbosity level")]
+    #[clap(short, long, parse(from_occurrences), help = "Sets verbosity level")]
     verbose: usize,
 
-    #[clap(last = true)]
-    targets: Vec<String>
+	#[clap(short, long, requires = "format_group", requires = "template_group", help = "Generate config file")]
+    generate: bool,
+
+    targets: Vec<String>,
 }
 
 #[derive(ArgEnum, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 enum Format {
-	Json,
+    Json,
+    Ron,
     Toml,
+    Url,
+    Xml,
     Yaml,
 }
 
-#[derive(Subcommand, Debug)]
-enum Command {
-    /// Generates a config file
-    Generate,
-    /// Edits the config file
-    Edit,
+#[derive(ArgEnum, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum Template {
+	Cmake,
+    Cargo,
+    Go,
+    Node,
+	Make,
+    Python,
 }
 
 fn main() {
-	let _args = Args::parse();
+    let args = Args::parse();
 
-    
-	
+    if let Some(format) = args.format {
+		println!("{:?}", format);
+    }
+
+    if let Some(template) = args.template {
+    	println!("{:?}", template);
+    }
+
+    println!("{}", args.verbose);
 }

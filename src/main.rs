b61325199bca2a8c100263e679980b0ef28e6cf3
Brian Reece <bdreece@mtu.edu>
2022-03-08 12:21:51 -0500

    Implemented verbose output messages

--- src/main.rs
+++ src/main.rs
@@ -28,22 +28,36 @@ fn main() -> Result<()> {
     // Parse config data into memory
     let config = match args.generate {
 		true => {
+            if args.verbose > 0 {
+            	println!("Generating config file");
+            }
             let c = Config::new(&path, &format, template).unwrap();
             c.dump(&format).unwrap();
             c
         },
-        false => Config::try_parse_from(&path).unwrap(),
+        false => {
+            if args.verbose > 0 {
+            	println!("Found config file");
+            }
+            Config::try_parse_from(&path).unwrap()
+        }
     };
 
     // Convert and exit
     if args.convert {
+        if args.verbose > 0 {
+        	println!("Converting and dumping config file");
+        }
     	config.dump(&format).unwrap();
         return Ok(());
     }
 
     // Parse config data per syntax
     let aliases = Aliases::try_parse_from(config).unwrap();
-    
+   	if args.verbose > 0 {
+    	println!("Successfully parsed aliases from config file");
+    }
+
     // Parse input script into memory
     let mut invocs: Invocs = vec![];
     if let Some(invoc_str) = &args.input {
@@ -52,10 +66,18 @@ fn main() -> Result<()> {
     	invocs = Invocs::try_parse_from(invoc_path).unwrap();	
     }
 
+    if args.verbose > 0 {
+    	println!("Successfully parsed invocations from script");
+    }
+
     // Match invocations to aliases and generate list of commands
     let mut commands: Commands = vec![];
     for invoc in &invocs {
     	commands.extend(Commands::try_parse_from((&mut invoc.clone(), &mut aliases.clone())).unwrap());
+    }
+
+    if args.verbose > 0 {
+    	println!("Successfully parsed commands from aliases and invocations");
     }
 	/*
 	println!("Aliases: {:#?}", aliases);
@@ -64,11 +86,15 @@ fn main() -> Result<()> {
 	*/
 	for command in commands {
     	let mut argv = command.split(" ");
-        Command::new(argv.next().unwrap())
-        	  			   .args(argv)
-                           .stdout(Stdio::inherit())
-                           .output()
-                           .expect("Failed to execute command");
+        let program = argv.next().unwrap();
+        if args.verbose > 0 {
+        	println!("Executing command: {}", program);
+        }
+        Command::new(program)
+        	.args(argv)
+            .stdout(Stdio::inherit())
+            .output()
+            .expect("Failed to execute command");
     }
 
     Ok(())
